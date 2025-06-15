001582f8b4b845f690b242a8151f11a66548cf5f
dangminhchau80@gmail.com <dangminhchau80@gmail.com>
2025-06-15 11:49:54 +0700

    xoa user dto

--- src/user/users/dto/requests/index.ts
+++ src/user/users/dto/requests/index.ts
@@ -2,5 +2,3 @@
 export { CreateUserDto } from './create-user.dto';
 export { UpdateUserDto } from './update-user.dto';
 export { UserQueryDto } from './user-query.dto';
-export { ChangeRoleDto } from './change-role.dto';
-export { ToggleActiveDto } from './toggle-active.dto';
